015c87c74eec4b518c36f4ac037fa5e4267f2ff0
bruno-duraes <brunodur4es@gmail.com>
2024-05-13 21:53:52 -0300

    Cria a entidade Medico

---
+++ src/main/java/med/vol/api/medico/MedicoEntity.java
@@ -0,0 +1,38 @@
+package med.vol.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import med.vol.api.endereco.EnderecoEntity;
+
+@Table(name = "medico")
+@Entity(name = "MedicoEntity")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class MedicoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private EEspecialidade especialidade;
+
+    @Embedded
+    private EnderecoEntity endereco;
+
+    public MedicoEntity(CadastroMedicoDTO cadastroMedicoDTO) {
+        this.nome = cadastroMedicoDTO.getNome();
+        this.email = cadastroMedicoDTO.getEmail();
+        this.crm = cadastroMedicoDTO.getCrm();
+        this.especialidade = cadastroMedicoDTO.getEspecialidade();
+        this.endereco = new EnderecoEntity(cadastroMedicoDTO.getEndereco());
+    }
+}
