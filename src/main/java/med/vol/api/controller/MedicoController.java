ddd2d33f5f097392e8ca533e83c75680e08f6791
bruno-duraes <brunodur4es@gmail.com>
2024-05-13 21:55:41 -0300

    Adiciona a lógica para cadastrar o medico no banco de dados

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.vol.api.controller;
 
 import med.vol.api.medico.CadastroMedicoDTO;
+import med.vol.api.medico.MedicoEntity;
+import med.vol.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    MedicoRepository medicoRepository;
+
     @PostMapping
     public void cadastrar(@RequestBody CadastroMedicoDTO cadastroMedicoDTO){
-        System.out.println(cadastroMedicoDTO);
+        medicoRepository.save(new MedicoEntity(cadastroMedicoDTO));
     }
 }
