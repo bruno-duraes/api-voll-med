7f396569ebaa6a8c9f25bf42b9807941ba569011
bruno-duraes <brunodur4es@gmail.com>
2024-05-21 22:19:10 -0300

    Adiciona as validações na DTO do paciente.

--- src/main/java/med/vol/api/paciente/CadastroPacienteDTO.java
+++ src/main/java/med/vol/api/paciente/CadastroPacienteDTO.java
@@ -1,16 +1,30 @@
 package med.vol.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import lombok.NonNull;
 import med.vol.api.endereco.EnderecoDTO;
 
 @Data
 public class CadastroPacienteDTO {
 
+    @NotBlank
     private String nome;
+
+    @Email
+    @NotBlank
     private String email;
-    private Long cpf;
-    private Long telefone;
+
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter exatamente 11 dígitos")
+    private String cpf;
+
+    @NotBlank
+    private String telefone;
+
+    @NotNull @Valid
     private EnderecoDTO endereco;
 
 }
