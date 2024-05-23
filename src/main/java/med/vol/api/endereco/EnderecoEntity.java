8d3c4f0e4bbd1afb3a7aff17a6485df8962d9870
bruno-duraes <brunodur4es@gmail.com>
2024-05-22 22:10:34 -0300

    Implementa o método de atualização de endereços.

--- src/main/java/med/vol/api/endereco/EnderecoEntity.java
+++ src/main/java/med/vol/api/endereco/EnderecoEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 @Embeddable
@@ -27,5 +28,30 @@ public class EnderecoEntity {
         this.complemento = endereco.getComplemento();
         this.cidade = endereco.getCidade();
         this.uf = endereco.getUf();
+    }
+
+    public void atualizarEndereco(EnderecoDTO endereco) {
+
+        if(StringUtils.isNotBlank(endereco.getLogradouro())){
+            this.logradouro = endereco.getLogradouro();
+        }
+        if(StringUtils.isNotBlank(endereco.getBairro())){
+            this.bairro = endereco.getBairro();
+        }
+        if(StringUtils.isNotBlank(endereco.getCep())){
+            this.cep = endereco.getCep();
+        }
+        if(endereco.getNumero() != null){
+            this.numero = endereco.getNumero();
+        }
+        if(StringUtils.isNotBlank(endereco.getComplemento())){
+            this.complemento = endereco.getComplemento();
+        }
+        if(StringUtils.isNotBlank(endereco.getCidade())){
+            this.cidade = endereco.getCidade();
+        }
+        if(StringUtils.isNotBlank(endereco.getUf())){
+            this.uf = endereco.getUf();
+        }
     }
 }
